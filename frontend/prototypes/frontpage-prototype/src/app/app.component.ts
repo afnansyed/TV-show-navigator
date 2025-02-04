854c2990d138ac7bf28cfa1d897a024b20b485d9
Joshua Yang <joshuayang@mac.com>
2025-02-03 23:32:34 -0500

    fixed the routing and slightly refine the code

--- frontend/prototypes/frontpage-prototype/src/app/app.component.ts
+++ frontend/prototypes/frontpage-prototype/src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MATERIAL_IMPORTS } from './material.imports';
-import { HomeComponent } from "./pages/home/home.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MATERIAL_IMPORTS, HomeComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 
