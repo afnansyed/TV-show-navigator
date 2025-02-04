9644da4a73ca91f948a7e4978f4273517575279f
Afnan Syed <afnansyed@ufl.edu>
2025-02-03 20:37:34 -0500

    added router to sign in page. moved componenets to homepage

--- frontend/prototypes/frontpage-prototype/src/app/app.routes.ts
+++ frontend/prototypes/frontpage-prototype/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'sign-in', component: SignInComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' } // Optional default route
+];
--- frontend/prototypes/frontpage-prototype/src/app/pages/home/home.component.ts
+++ frontend/prototypes/frontpage-prototype/src/app/pages/home/home.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { MATERIAL_IMPORTS } from '../../material.imports'; 
+import { FeaturesComponent } from '../../components/features/features.component'; 
+import { HeroComponent } from '../../components/hero/hero.component'; 
+import { NavbarComponent } from '../../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterOutlet, CommonModule, MATERIAL_IMPORTS, FeaturesComponent, HeroComponent, NavbarComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
+  template: `
+    
+  <router-outlet></router-outlet>
+  `
+
 })
+
 export class HomeComponent {
 
 }
